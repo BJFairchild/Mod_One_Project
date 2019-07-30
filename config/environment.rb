2e0d06f2ad405f2adc6531730971892fe014acdd
github username <github email address>
2019-07-29 17:33:16 -0700

    Updated console, rakefile, environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,5 @@
-require 'pry'
-require_all 'lib'
-require 'bundler/setup'
+require 'bundler'
 Bundler.require
-require 'rake'
-require 'active_record'
+
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
+require_all 'lib'
