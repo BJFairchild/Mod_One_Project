97675ba6aa919cfd1159ba0b12ee5c502692557b
github username <github email address>
2019-07-29 17:53:54 -0700

    end of day 1

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ Bundler.require
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'db/development.db')
 require_all 'lib'
+
+#https://opentdb.com/api.php?amount=25&difficulty=easy&type=multiple
+# API LINK
