c5aec775f9cbfa9ae0650dda34d5aef4579172b9
github username <github email address>
2019-08-01 11:48:21 -0700

    wat

--- bin/api_communicator.rb
+++ bin/api_communicator.rb
@@ -6,7 +6,7 @@ require_relative '../lib/question.rb'
 
 class APICommunicator
     def self.load
-        200.times do
+        400.times do
             self.generate
         end
     end
@@ -16,11 +16,11 @@ class APICommunicator
             m_q = JSON.parse(RestClient.get("https://opentdb.com/api.php?amount=1&type=multiple"))
 
             Question.create(
-                question_text: m_q["results"][0]["question"].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", ""), 
-                correct_answer: m_q["results"][0]["correct_answer"].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", ""), 
-                incorrect_answer: m_q["results"][0]["incorrect_answers"][0].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", ""), 
-                incorrect_answer_two: m_q["results"][0]["incorrect_answers"][1].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", ""), 
-                incorrect_answer_three: m_q["results"][0]["incorrect_answers"][2].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", ""), 
+                question_text: m_q["results"][0]["question"].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", "").gsub("&egrave;", "e"), 
+                correct_answer: m_q["results"][0]["correct_answer"].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", "").gsub("&egrave;", "e"), 
+                incorrect_answer: m_q["results"][0]["incorrect_answers"][0].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", "").gsub("&egrave;", "e"), 
+                incorrect_answer_two: m_q["results"][0]["incorrect_answers"][1].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", "").gsub("&egrave;", "e"), 
+                incorrect_answer_three: m_q["results"][0]["incorrect_answers"][2].gsub("&quot;", '"').gsub("&#039;", "'").gsub("&eacute;", "e").gsub("&amp;", "&").gsub("&oacute;", "o").gsub("&aacute;", "a").gsub("&ouml;", "o").gsub("&aring;", "a").gsub("&auml;", "a").gsub("&ntilde;", "n").gsub("&shy;", "").gsub("&egrave;", "e"), 
                 catagory: m_q["results"][0]["category"],
                 difficulty: m_q["results"][0]["difficulty"]
                 )
